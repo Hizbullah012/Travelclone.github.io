84f6adba57aa7b04262f8da5ffcfb058ec9ad4dd
Hizbullah <hizbullahmemon916@gmail.com>
2023-06-06 22:16:06 +0500

    Add New files

---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+
+
+// javacript for preloader
+
+const preloader = document.querySelector("[data-preloader]");
+window.addEventListener("load",()=>{
+    preloader.classList.add("remove")
+})
+
+// add event on multiple element
+
+
+const addEventOnElements= function(elements,eventType,callBack){
+    for(let i=0 ,len = elements.length;i<len;i++){
+       elements[i].addEventListener(eventType,callBack);
+
+    }
+}
+// Navbar toggler
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]")
+const overlay = document.querySelector("[data-overlay]");
+
+
+const toggleNav = function(){
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+    document.body.classList.toggle("nav-active");
+}
+addEventOnElements(navTogglers,"click",toggleNav);
+
+// header
+
+const header = document.querySelector("[data-header]");
+window.addEventListener("scroll" , function(){
+    header.classList[window.scrollY > 100 ? "add" : "romove"]("active");
+})
